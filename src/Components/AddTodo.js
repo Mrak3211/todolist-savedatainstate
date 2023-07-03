e427336765ea5b6d7da3a6a8fb3369e4eee2087d
Ayush <ayushchodvadiya9898@gmail.com>
2023-07-03 16:32:44 +0530

    added alert, set task complete time, added search bar with working

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 import "./AddTodo.css";
-// eslint-disable-next-line
 import {
   addTodo,
   deleteTodo,
@@ -14,29 +13,45 @@ const AddTodo = () => {
   const [todo, setTodo] = useState({
     title: "",
     todo: "",
+    completionTime: "",
   });
   const [updateTodo, setUpdateTodo] = useState(null);
   const newId = uuidv4();
   const todoObj = useSelector((state) => state.todoObj);
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    if (updateTodo) {
-      const updatedTodo = { ...todo, id: updateTodo };
-      dispatch(updateTodoAction(updatedTodo));
-      setUpdateTodo(null);
-    } else {
-      const newTodo = { ...todo, id: newId };
-      dispatch(addTodo(newTodo));
+  const handleSubmit = () => {
+    if (todo.title.trim() !== "" && todo.todo.trim() !== "") {
+      if (updateTodo) {
+        const updatedTodo = { ...todo, id: updateTodo };
+        dispatch(updateTodoAction(updatedTodo));
+        setUpdateTodo(null);
+        const updatedTodoObj = todoObj.map((obj) =>
+          obj.id === updateTodo ? updatedTodo : obj
+        );
+        const store = JSON.stringify(updatedTodoObj);
+        localStorage.setItem("todo", store);
+      } else {
+        const newTodo = { ...todo, id: newId, completed: false };
+        dispatch(addTodo(newTodo));
+        const store = JSON.stringify([...todoObj, newTodo]);
+        localStorage.setItem("todo", store);
+      }
+      setTodo({
+        title: "",
+        todo: "",
+        completionTime: "",
+      });
+      checkExpiredTodos();
     }
-    setTodo({
-      title: "",
-      todo: "",
-    });
   };
 
   const handleDelete = (id) => {
     dispatch(deleteTodo(id));
+    const updatedTodoObj = todoObj.filter((object) => object.id !== id);
+    const store = JSON.stringify(updatedTodoObj);
+    localStorage.setItem("todo", store);
+    checkExpiredTodos();
   };
 
   const handleUpdate = (todo) => {
@@ -44,10 +59,63 @@ const AddTodo = () => {
     setTodo({
       title: todo.title,
       todo: todo.todo,
+      completionTime: todo.completionTime,
     });
   };
+
+  // eslint-disable-next-line
+  const storedTodos = JSON.parse(localStorage.getItem("todo")) || [];
+
+  const checkExpiredTodos = () => {
+    const currentTime = new Date().getTime();
+    const storedTodos = JSON.parse(localStorage.getItem("todo")) || [];
+    storedTodos.forEach((todo) => {
+      const completionTime = new Date(todo.completionTime).getTime();
+      const timeDifference = (completionTime - currentTime) / 1000;
+      if (timeDifference <= 0 && !todo.completed) {
+        alert(`Todo "${todo.todo}" has been completed ?`);
+        todo.completed = true;
+      }
+    });
+
+    const updatedStoredTodos = JSON.stringify(storedTodos);
+    localStorage.setItem("todo", updatedStoredTodos);
+  };
+
+  useEffect(() => {
+    checkExpiredTodos();
+  }, []);
+
+  const [searchResult, setSearchResult] = useState("");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    const trimmedSearchResult = searchResult.trim();
+    if (trimmedSearchResult !== "") {
+      const searchResults = storedTodos.filter((todo) =>
+        todo.title.includes(trimmedSearchResult)
+      );
+      setFilteredTodos(searchResults);
+    } else {
+      setFilteredTodos([]);
+    }
+  }, [searchResult]);
+
+
   return (
     <>
+      <div className="container my-3" style={{ maxInlineSize: "fit-content" }}>
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={searchResult}
+            onChange={(e) => setSearchResult(e.target.value)}
+          />
+        </form>
+      </div>
       <div className="container">
         <div className="mb-3">
           <label htmlFor="title" className="form-label">
@@ -74,6 +142,20 @@ const AddTodo = () => {
             onChange={(e) => setTodo({ ...todo, todo: e.target.value })}
           />
         </div>
+        <div className="mb-3">
+          <label htmlFor="completionTime" className="form-label">
+            Completion Time
+          </label>
+          <input
+            type="datetime-local"
+            className="form-control"
+            id="completionTime"
+            value={todo.completionTime}
+            onChange={(e) =>
+              setTodo({ ...todo, completionTime: e.target.value })
+            }
+          />
+        </div>
         <button
           type="submit"
           disabled={
@@ -86,27 +168,29 @@ const AddTodo = () => {
         </button>
       </div>
       <div className="container">
-        {todoObj.map((object) => (
-          <div key={object.id} className="card my-3">
-            <Todos todos={object} />
-            <div className="card-body">
-              <button
-                type="button"
-                className="btn btn-danger mx-1 my-1"
-                onClick={() => handleDelete(object.id)}
-              >
-                Delete
-              </button>
-              <button
-                type="button"
-                className="btn btn-success mx-1 my-1"
-                onClick={() => handleUpdate(object)}
-              >
-                Update
-              </button>
+        {(filteredTodos.length > 0 ? filteredTodos : storedTodos).map(
+          (object) => (
+            <div key={object.id} className="card my-3">
+              <Todos todos={object} searchh={filteredTodos} />
+              <div className="card-body">
+                <button
+                  type="button"
+                  className="btn btn-danger mx-1 my-1"
+                  onClick={() => handleDelete(object.id)}
+                >
+                  Delete
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-success mx-1 my-1"
+                  onClick={() => handleUpdate(object)}
+                >
+                  Update
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </>
   );
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 const Todos = (props) => {
-  const { title, todo, id } = props.todos;
+  const { title, todo, id, completionTime } = props.todos;
+
   return (
-    <div className="container my-3" style={{ display: "flex" }}>
-      <div className="card" style={{ width: "18rem", marginRight: "1rem" }}>
-        <div className="card-body">
-          <h5 className="card-title my-1">Title: {title}</h5>
-          <h6 className="card-subtitle mb-2 text-muted my-1">Todo: {todo}</h6>
-          <h6 className="card-subtitle mb-2 text-muted my-1">
-            Todo Id: <b>{id}</b>
-          </h6>
+    <>
+      <div className="container my-3" style={{ display: "flex" }}>
+        <div className="card" style={{ width: "18rem", marginRight: "1rem" }}>
+          <div className="card-body">
+            <h5 className="card-title my-1">Title: {title}</h5>
+            <h6 className="card-subtitle mb-2 text-muted my-1">Todo: {todo}</h6>
+            <h6 className="card-subtitle mb-2 text-muted my-1">
+              Todo Id: <b>{id}</b>
+            </h6>
+            <h6 className="card-subtitle mb-2 text-muted my-1">
+              Completion Time: <b>{completionTime}</b>
+            </h6>
+            {/* <i className="fa-regular fa-clock" style={{ cursor: "pointer" }}></i> */}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
